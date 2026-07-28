b5c989fe15407871ed4f60231e14912d0497e26e
ChiranshiParekh <chiranshiparekh@gmail.com>
2026-07-28 19:42:32 +0530

    chore: initialize project structure

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("===================================")
+    print(" Smart Cold Mailer")
+    print(" Version: 1.0.0")
+    print("===================================")
+
+
+if __name__ == "__main__":
+    main()
